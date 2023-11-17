8bf56827a20c3685b0beb7ed7c267e36eb8cb0c6
Anthax386 <kevinanthax@protonmail.com>
2023-11-17 12:26:19 +0100

    add option for stay logged

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -5,36 +5,54 @@ function LogIn() {
         event.preventDefault();
 
         // Récupération des input
+        const $stayLogged = event.target.querySelector('[name=stayLogged]').value;
         const loginValue = {
             email: event.target.querySelector('[name=e-mail]').value,
             password: event.target.querySelector('[name= password]').value,
         }
 
-        // Création charge utile au format JSON
-        const chargeUtile = JSON.stringify(loginValue);
-        
-        const reponse= await fetch('http://localhost:5678/api/users/login', {
-            method: "POST",
-            headers: {"Content-Type":"application/json"},
-            body: chargeUtile,
-        });
+        async function getToken(){
+            const chargeUtile = JSON.stringify(loginValue);
 
-        //Stokage de la réponse dans constante
-        const user = await reponse.json();
-        if (user.userId) {
+            const reponse= await fetch('http://localhost:5678/api/users/login', {
+                method: "POST",
+                headers: {"Content-Type":"application/json"},
+                body: chargeUtile,
+            });
+            const user = await reponse.json();
+            return user;
+        };
+        getToken();
+        const user = await getToken();
+
+        async function stayLogged(){
             window.localStorage.setItem("userId", user.userId);
             window.localStorage.setItem('token', user.token);
+            location.href='./index.html';
+        }
+
+        async function dontStayLogged(){
+            window.sessionStorage.setItem("userId", user.userId);
+            window.sessionStorage.setItem('token', user.token);
 
             location.href='./index.html';
-        }else {
-            const loginError = document.querySelector('.loginError');
-            loginError.classList.remove('hidden');
+        }
 
-            const loginErrorBtn = document.getElementById('loginErrorBtn');
-            loginErrorBtn.addEventListener('click', function(){
-                loginError.classList.add('hidden');
-            });
-        };
+        if (user.userId) {   
+            if($stayLogged === true) {
+                stayLogged();
+            } else {
+                dontStayLogged();
+            }
+        } else {
+            const loginError = document.querySelector('.loginError');
+                loginError.classList.remove('hidden');
+    
+                const loginErrorBtn = document.getElementById('loginErrorBtn');
+                loginErrorBtn.addEventListener('click', function(){
+                    loginError.classList.add('hidden');
+                });
+        };                
     });
 };
 
