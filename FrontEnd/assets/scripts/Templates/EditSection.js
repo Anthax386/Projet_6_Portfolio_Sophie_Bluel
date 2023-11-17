efd57775134b40783cda0e82324fbddc90ea2069
Anthax386 <kevinanthax@protonmail.com>
2023-11-17 18:31:04 +0100

    edit function for admin popup

--- FrontEnd/assets/scripts/Templates/EditSection.js
+++ FrontEnd/assets/scripts/Templates/EditSection.js
@@ -1,17 +1,36 @@
 const $mainContainer = document.getElementById('main');
+import {createNewWorkForm} from "../Templates/AddingNewWork.js"
+import {createAdminGalery} from "../Functions/CreateAdminGallery.js"
 
 export function createEditSection(){
-    const $fadedBackground = document.createElement('div');
-        $fadedBackground.classList.add('blackback');
-    $mainContainer.appendChild($fadedBackground);
+    const $fadedBackground = document.querySelector('#blackback');
+        $fadedBackground.classList.remove('hidden');
 
-    const $editSection = document.createElement('section');
-        $editSection.classList.add('editSection');
+    const $editSection = document.querySelector('#editSection');
+        $editSection.classList.remove('hidden');
         $editSection.innerHTML = `
             <h2 id="editSectionTitle"></h2>
             <button class="closeBtn"><i class="fa-solid fa-xmark fa-xl"></i></button>
-            <div id="editSectionGalerie"></div>
+            <div id="editSectionGalerie" class="editSectionGalerie"></div>
             <button class="btn greenBtn" id="addPictureBtn">Ajouter une photo</button>
         `;
-    $mainContainer.appendChild($editSection);
+
+    const $editSectionTitle = document.querySelector('#editSectionTitle');
+            $editSectionTitle.innerText = 'Galerie photo';
+
+    const $closeBtn = document.querySelector('.closeBtn');
+        $closeBtn.addEventListener('click', function(){
+            $fadedBackground.classList.add("hidden");
+            $editSection.classList.add("hidden");
+        });
+    
+    const $editSectionGalerie = document.querySelector('#editSectionGalerie');
+    const $addPictureBtn = document.querySelector('#addPictureBtn');
+        $addPictureBtn.addEventListener('click', function(){
+            $editSectionGalerie.remove();
+            $editSectionTitle.innerText = 'Ajout photo';
+            createNewWorkForm();
+        });
+
+    createAdminGalery();
 };
--- FrontEnd/assets/scripts/logged.js
+++ FrontEnd/assets/scripts/logged.js
@@ -12,30 +12,5 @@ export function isLogged(){
     $worksSection.appendChild($editBtn);
 
     $editBtn.addEventListener('click', function(){
-
         createEditSection();
-        const $editSectionGalerie = document.querySelector('#editSectionGalerie');
-
-        // Création de l'element titre de la section
-        const $editSectionTitle = document.querySelector('#editSectionTitle');
-            $editSectionTitle.innerText = 'Galerie photo';
-
-        createAdminGalery();
-
-        const $addPictureBtn = document.querySelector('#addPictureBtn');
-            $addPictureBtn.addEventListener('click', function(){
-                $editSectionGalerie.remove();
-                $editSectionTitle.innerText = 'Ajout photo';
-                
-                createNewWorkForm();
-            });
-
-        // Ajout du bouton pour fermer le popup
-        const $closeBtn = document.querySelector('.closeBtn');
-        $closeBtn.addEventListener('click', function(){
-            const $fadedBackground = document.querySelector(".blackback");
-            const $editSection = document.querySelector(".editSection");
-            $fadedBackground.remove();
-            $editSection.remove();
-        });
 })};
